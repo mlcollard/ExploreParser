dc64a5f5eed29191625f6cd7d7770006c617343d
Michael L. Collard <collard@uakron.edu>
2021-08-25 13:08:20 -0400

    Add initial files

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/*
+    hello.cpp
+
+    A hello world for an action
+*/
+
+#include <iostream>
+
+int main() {
+
+	std::cout << "Hello Action!\n";
+
+	return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+    main.cpp
+
+    A do-nothing program to investigate compiler stages
+*/
+
+#include "function.hpp"
+
+int main() {
+
+    return 0;
+}
